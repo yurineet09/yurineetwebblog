6a765fc1fec3312b348e7f65ed55782a8a2eb6b7
yurineet09 <yurineet09@gmail.com>
2026-07-14 15:15:42 -0300

    version 5.4.3

---
+++ yurineetchan5.4.3/assets/scripts/js/sketch-data.js
@@ -0,0 +1,198 @@
+/* ─────────────────────────────────────────────────────────────────────────
+   sketch-data.js
+   The sizeKB / width / height / format fields are filled in automatically
+   when running:  node generate-gallery-data.js
+   While they are null, gallery.html uses fetch as a fallback.
+───────────────────────────────────────────────────────────────────────── */
+(function(){
+  'use strict';
+
+  var sketches = [
+    {
+      file:        'alienmaldito.png',
+      title:       'Sketch Alien Hominid',
+      description: 'aliennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn',
+      date:        '05/05/26(Tue)18:10:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'choppedmyself.png',
+      title:       'Chopped Myself',
+      description: 'cut and taped back together.',
+      date:        '05/05/26(Tue)18:02:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'rikafurude.png',
+      title:       'Neko Rika',
+      description: 'nipah~',
+      date:        '05/05/26(Tue)17:55:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'dante2.png',
+      title:       'Sketch Dante 2',
+      description: 'kewl sworddd',
+      date:        '05/05/26(Tue)17:41:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'dante3.png',
+      title:       'Sketch Dante 1',
+      description: 'boooooriiinggg',
+      date:        '05/05/26(Tue)17:35:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'coelho5.png',
+      title:       'Do NOT let em in',
+      description: 'Army of violent flesh-eating bunnies outside',
+      date:        '05/05/26(Tue)17:20:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'coelho4.png',
+      title:       'You Against They',
+      description: "Kill all 'They...HERE:\u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e \u200e https://www.roblox.com/pt/games/93329401639398/they'",
+      date:        '05/05/26(Tue)17:14:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'coelho3.png',
+      title:       'Bunny MF & MF Bunny',
+      description: 'It is heavily inspired by that one blue and yellow gay love or whatever this is https://x.com/sometimes317/status/1861971961259184166',
+      date:        '05/05/26(Tue)17:06:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'coelho2.png',
+      title:       'MF Bunny',
+      description: 'Lock the doors and close the blinds',
+      date:        '05/05/26(Tue)16:58:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'nowyouknowhowitfeelslike.png',
+      title:       'Now You know how it feels like',
+      description: 'Never pet This.',
+      date:        '05/05/26(Tue)16:39:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'princessunicorn.png',
+      title:       'Princess Unicorn Bunny Kitten Angel the most beautifulest in the whole wide world',
+      description: 'Love laugh bunnies 4ever.',
+      date:        '05/05/26(Tue)16:27:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'easter.jpg',
+      title:       'Happy Easter PCDJ!',
+      description: 'What the bunny brings you?',
+      date:        '05/05/26(Tue)16:14:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'JPEG'
+    },
+    {
+      file:        'mishanshizune.jpg',
+      title:       'Misha and Shizune from Katawa Shoujo',
+      description: 'Lesbian ahhh',
+      date:        '05/05/26(Tue)16:02:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'JPEG'
+    },
+    {
+      file:        'lerolero.png',
+      title:       'Lero lero',
+      description: 'lerolerolerolerolero',
+      date:        '05/05/26(Tue)15:51:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    },
+    {
+      file:        'demon.png',
+      title:       'Stare',
+      description: '&lt;O&gt; &lt;O&gt;',
+      date:        '05/05/26(Tue)15:40:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    }
+  ];
+
+  var pictures = [
+    {
+      file:        'coelho1.png',
+      title:       'MF Bunny rig',
+      description: "A fail rig i made using my cat's fur texture.",
+      date:        '05/05/26(Tue)16:50:00',
+      author:      'Settembre',
+      sizeKB:      null,
+      width:       null,
+      height:      null,
+      format:      'PNG'
+    }
+  ];
+
+  window.YURINEET_GALLERY = {
+    sketches: sketches,
+    pictures: pictures
+  };
+  window.YURINEET_SKETCHES = sketches;
+  window.YURINEET_PICTURES = pictures;
+})();
